5e36c6ed5184da7f22a2f380e44fd4c9970cf22e
Andrew <andrew@Shepherds-MacBook-Pro.local>
2021-02-05 20:19:48 +0800

    init

---
+++ dist/ts-lib.esm.js
@@ -0,0 +1,2 @@
+console.log("test");
+//# sourceMappingURL=ts-lib.esm.js.map
---
+++ dist/ts-lib.umd.js
@@ -0,0 +1,2 @@
+!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){"use strict";console.log("test")}));
+//# sourceMappingURL=ts-lib.umd.js.map
